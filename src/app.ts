0762ce8013e1b7f8d0f146ce211dcb999bf7ead5
Khoa <khoa@192.168.1.34>
2025-01-05 12:14:40 +0700

    add node express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('helloworld')
+import { envs } from './core/config/env';
+import { Server } from './server';
+
+(() => {
+ main();
+})();
+
+function main(): void {
+ const server = new Server({
+  port: envs.PORT,
+ });
+
+ void server.start();
+}
